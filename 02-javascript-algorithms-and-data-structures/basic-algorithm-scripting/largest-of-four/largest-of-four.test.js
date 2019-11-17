fb2ff6150dee57ebc6d3bf14e1227b6735107bb7
Marco Biedermann <marco.biedermann@gmx.de>
2019-11-17 16:07:28 +0100

    chore: Dependencies
    Update dependencies

--- 02-javascript-algorithms-and-data-structures/basic-algorithm-scripting/largest-of-four/largest-of-four.test.js
+++ 02-javascript-algorithms-and-data-structures/basic-algorithm-scripting/largest-of-four/largest-of-four.test.js
@@ -3,7 +3,12 @@ import largestOfFour from './largest-of-four';
 describe('largestOfFour()', () => {
   it('largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array', () => {
     const expected = Array.isArray(
-      largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]),
+      largestOfFour([
+        [4, 5, 1, 3],
+        [13, 27, 18, 26],
+        [32, 35, 37, 39],
+        [1000, 1001, 857, 1],
+      ]),
     );
     const actual = true;
 
@@ -11,21 +16,36 @@ describe('largestOfFour()', () => {
   });
 
   it('largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27, 5, 39, 1001]', () => {
-    const expected = largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+    const expected = largestOfFour([
+      [13, 27, 18, 26],
+      [4, 5, 1, 3],
+      [32, 35, 37, 39],
+      [1000, 1001, 857, 1],
+    ]);
     const actual = [27, 5, 39, 1001];
 
     expect(expected).toEqual(actual);
   });
 
   it('largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000]', () => {
-    const expected = largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]);
+    const expected = largestOfFour([
+      [4, 9, 1, 3],
+      [13, 35, 18, 26],
+      [32, 35, 97, 39],
+      [1000000, 1001, 857, 1],
+    ]);
     const actual = [9, 35, 97, 1000000];
 
     expect(expected).toEqual(actual);
   });
 
   it('largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) should return [25, 48, 21, -3]', () => {
-    const expected = largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
+    const expected = largestOfFour([
+      [17, 23, 25, 12],
+      [25, 7, 34, 48],
+      [4, -10, 18, 21],
+      [-72, -3, -17, -10],
+    ]);
     const actual = [25, 48, 21, -3];
 
     expect(expected).toEqual(actual);
